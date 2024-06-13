1d5070ecd7980c9fd8dcfab2b854aacdaf4f36b8
jiri.prajz <jiri.prajz@advantages.cz>
2024-06-13 18:56:38 +0200

    Added new function convert. Input parameters: picture, width, height.

--- src/Origam.QrCode/QRCodeServiceAgent.cs
+++ src/Origam.QrCode/QRCodeServiceAgent.cs
@@ -18,8 +18,11 @@ You should have received a copy of the GNU General Public License
 along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 */
 #endregion
+using ImageMagick;
+using ImageMagick.Formats;
 using Origam.Service.Core;
 using QRCoder;
+using System;
 using System.Collections;
 
 namespace Origam.QRCode;
@@ -43,6 +46,12 @@ public class QRCodeServiceAgent : IExternalServiceAgent
                 }
                 Result = Create(text, pixelsPerModule);
                 break;
+            case "Convert":
+                byte[] data = Parameters.Get<byte[]>("Data");
+                int width = Parameters.Get<int>("Width");
+                int height = Parameters.Get<int>("Height");
+                Result = FixedSizeBytes(data, width, height);
+                break;
         }
     }
     private static object Create(string inputText, int pixelsPerModule)
@@ -51,5 +60,43 @@ public class QRCodeServiceAgent : IExternalServiceAgent
         QRCodeData qrCodeData = qrGenerator.CreateQrCode(inputText, QRCodeGenerator.ECCLevel.Q);
         BitmapByteQRCode qrCode = new(qrCodeData);
         return qrCode.GetGraphic(pixelsPerModule);
+    }
+    private static byte[] FixedSizeBytes(byte[] byteArrayImage, int width, int height)
+    {
+        var bmpReadDefines = new BmpReadDefines
+        {
+            IgnoreFileSize = true
+        };
+
+        // Write to stream
+        var settings = new MagickReadSettings();
+        settings.SetDefines(bmpReadDefines);
+        MagickImage imageInfo = new MagickImage(byteArrayImage, settings);
+        var sourceWidth = imageInfo.Width;
+        var sourceHeight = imageInfo.Height;
+        var destX = 0;
+        var destY = 0;
+        float percent;
+        var percentWidth = width / (float)sourceWidth;
+        var percentHeight = height / (float)sourceHeight;
+        if (percentHeight < percentWidth)
+        {
+            percent = percentHeight;
+            destX = Convert.ToInt16((width - sourceWidth * percent) / 2);
+        }
+        else
+        {
+            percent = percentWidth;
+            destY = Convert.ToInt16(
+                (height - sourceHeight * percent) / 2);
+        }
+        var destWidth = (int)(sourceWidth * percent);
+        var destHeight = (int)(sourceHeight * percent);
+        var backgroundImage = new MagickImage(MagickColors.Black, width, height);
+        backgroundImage.Resize(width, height);
+        var pictureBitmap = new MagickImage(byteArrayImage, settings);
+        pictureBitmap.Resize(destWidth, destHeight);
+        backgroundImage.Composite(pictureBitmap, destX, destY);
+        return backgroundImage.ToByteArray(imageInfo.Format);
     }
 }
